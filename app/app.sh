5c00751bfb1a0f59cc171797b3deb579a3c8dbe7
Jaime <santos.jaimemendes91@gmail.com>
2023-04-17 21:37:14 -0300

    Primeiro Commit

---
+++ app/app.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pip install bottle==0.12.13
+pip install psycopg2==2.8.5
+python -u sender.py
---
+++ app/sender.py
@@ -0,0 +1,17 @@
+from bottle import route, run, request
+import psycopg2
+ 
+DSN = 'dbname=email_sender user=postgres password=postgres host=db'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+ 
+@route('/', method='POST')
+def send():
+    assunto = request.forms.get('assunto')
+    mensagem = request.forms.get('mensagem')
+    
+    return 'Mensagem enfileirada! Assunto: {} Mensagem: {}'.format(
+        assunto, mensagem
+    )
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
